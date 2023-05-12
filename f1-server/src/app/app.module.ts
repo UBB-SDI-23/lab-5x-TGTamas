7735348cbc5c6b7403d1d68d7ba700dc51b62072
917_Tankó-Gábor_Tamás <tankogtamas@gmail.com>
2023-05-12 15:05:01 +0300

    Added responsive frontend

--- f1-server/src/app/app.module.ts
+++ f1-server/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { DriverDetailsComponent } from './components/driver-details/driver-detai
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule
   ],
   providers: [RestService, DriverService], 
   bootstrap: [AppComponent]
